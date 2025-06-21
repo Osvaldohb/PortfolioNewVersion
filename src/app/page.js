0040130605e52f54bc5e755cb7e73463d8d2175b
Osvaldo Santillan <osvaldonuevo@gmail.com>
2025-06-21 03:27:34 -0600

    Initial commit

--- src/app/page.js
+++ src/app/page.js
@@ -1,103 +1,222 @@
+'use client';
+
 import Image from "next/image";
+import CardTech from "./components/CardTech/CardTech";
+import { useState,useEffect, use } from "react";
 
 export default function Home() {
-  return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm/6 text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-[family-name:var(--font-geist-mono)] font-semibold">
-              src/app/page.js
-            </code>
-            .
-          </li>
-          <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
-          </li>
-        </ol>
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
+  const technologiesBackend = [
+  {
+    imgId: "11",
+    Name: "Node.js",
+    Description:
+      "A high-performance JavaScript runtime I use to build scalable backend services and APIs.",
+  },
+  {
+    imgId: "2",
+    Name: "Express.js",
+    Description:
+      "A minimal and flexible web framework for Node.js that helps me create robust API architectures.",
+  },
+  {
+    imgId: "3",
+    Name: "PostgreSQL",
+    Description:
+      "A powerful open-source relational database I use for structured, reliable data storage.",
+  },
+  {
+    imgId: "4",
+    Name: "MongoDB",
+    Description:
+      "A NoSQL document database I use to handle flexible and dynamic data structures.",
+  },
+  {
+    imgId: "6",
+    Name: "JWT",
+    Description:
+      "A secure method I use to handle authentication and authorization via token-based systems.",
+  },
+  {
+    imgId: "8",
+    Name: "REST & GraphQL",
+    Description:
+      "API paradigms I use to design clear and scalable communication between frontend and backend.",
+  },
+  {
+    imgId: "10",
+    Name: "Spring Boot",
+    Description:
+      "A Java-based framework I use for building production-ready backend applications with ease.",
+  },
+];
+
+    const technologies=[
+  {
+    imgId: "1",
+    Name: "React",
+    Description:
+      "A declarative JavaScript library I use to build fast, interactive, and reusable UI components.",
+  },
+  {
+    imgId: "2",
+    Name: "Next.js",
+    Description:
+      "A React framework that enables server-side rendering, API routes, and optimized frontend performance.",
+  },
+  {
+    imgId: "3",
+    Name: "Tailwind CSS",
+    Description:
+      "A utility-first CSS framework that helps me build precise and responsive designs faster.",
+  },
+  {
+    imgId: "4",
+    Name: "TypeScript",
+    Description:
+      "Typed JavaScript that enhances code reliability, scalability, and developer experience.",
+  },
+  {
+    imgId: "5",
+    Name: "JavaScript",
+    Description:
+      "The dynamic language of the web — I use it to build interactivity and application logic.",
+  },
+  {
+    imgId: "6",
+    Name: "HTML5",
+    Description:
+      "Semantic markup that forms the accessible and SEO-friendly structure of every interface.",
+  },
+  {
+    imgId: "7",
+    Name: "CSS3",
+    Description:
+      "The styling foundation of the web — used for animations, layouts, and visual polish.",
+  },
+  {
+    imgId: "8",
+    Name: "GSAP",
+    Description:
+      "A performance-focused animation library I use to create smooth, professional motion effects.",
+  },
+  {
+    imgId: "9",
+    Name: "Angular",
+    Description:
+      "A robust frontend framework I’ve used for scalable enterprise apps and strong architecture.",
+  },
+  {
+    imgId: "10",
+    Name: "Bootstrap",
+    Description:
+      "A UI toolkit I use for rapid prototyping and responsive design in admin-like interfaces.",
+  },
+  ]
+
+
+
+  const[currentIndex, setCurrentIndex] = useState(0);
+  const[currentIndexBackend, setCurrentIndexBackend] = useState(0);
+
+  useEffect(()=>{
+    const interval=setInterval(()=>{
+       setCurrentIndex((prev)=> prev === technologies.length - 1 ? 0 : prev + 1);
+    },5000)
+
+    return () => clearInterval(interval);
+  },[technologies.length])
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setCurrentIndexBackend((prev) => (prev === technologiesBackend.length - 1 ? 0 : prev + 1));
+    }, 5000);
+
+    return () => clearInterval(interval);
+  }, [technologiesBackend.length]);
+
+
+
+
+  return (
+    <>
+      <main className=" flex justify-center items-center px-16  h-screen bg-white hero">
+          <section className="flex flex-col  text-center gap-6 items-center ">
+            <h1 className="ApperAnimation text-xl sm:text-5xl md:text-6xl font-bold tracking-tight text-gray-900">Full Stack Developer <span className="bg-gradient-to-r from-purple-500 via-pink-500 to-red-500 bg-clip-text text-transparent">
+  Osvaldo Santillan
+</span>
+ </h1>
+          <h1 className="SideAnimation text-xl sm:text-2xl md:text-4xl font-bold tracking-tight text-gray-900">
+            Elevating digital experiences through  clean design and modern code.
+          </h1>
+          <h2 className="text-lg sm:text-xl md:text-2xl text-gray-600 max-w-2xl mt-5">
+            Crafting sleek, fast, and accessible interfaces that strengthen your brand and captivate users.
+          </h2>
+        </section>
+
+       
       </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
+
+    <section className="py-20 px-6 bg-gray-50 text-center  cardZone">
+        <h2 className="text-2xl md:text-3xl font-semibold text-gray-900 mb-8">
+          Technologies I use to build elegant web experiences
+        </h2>
+        <div className="flex justify-center relative ">
+          {technologies.map((tech, index) => (
+            <div 
+            key={index} 
+            className={`absolute transition-opacity duration-1000 ease-in-out ${
+            index === currentIndex ? 'animate-fade-in' : 'opacity-0 z-0'
+          }`}
+            id="techCard"
+            >
+              <CardTech
+                
+                imgId={tech.imgId}
+                Name={tech.Name}
+                Description={tech.Description}
+              />
+            </div>
+          ))}
+
+
+
+
+          
+        </div>
+
+
+
+      </section> 
+
+        {/* <section className="py-20 px-6 bg-gray-50 text-center  cardZone">
+        <h2 className="text-2xl md:text-3xl font-semibold text-gray-900 mb-8">
+           Technologies that power performance and scalability
+        </h2>
+        <div className="flex justify-center relative ">
+          {technologiesBackend.map((tech, index) => (
+            <div 
+            key={index} 
+            className={`absolute transition-opacity duration-1000 ease-in-out ${
+            index === currentIndexBackend ? 'animate-fade-in' : 'opacity-0 z-0'
+          }`}
+            id="techCard"
+            >
+              <CardTech
+                
+                imgId={tech.imgId}
+                Name={tech.Name}
+                Description={tech.Description}
+              />
+            </div>
+          ))}
+
+          
+        </div>
+
+
+      </section>  */}
+
+    </>
   );
 }
